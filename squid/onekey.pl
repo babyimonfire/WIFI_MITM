8a5ee3eb1fc35f48a95bc1fda499e501a0d6289b
babyimonfire <babyimonfire@163.com>
2016-11-14 21:19:59 -0500

    Create onekey.pl
    default and unchanged redirect program script

---
+++ squid/onekey.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+$|=1;
+$pid = $$;
+while (<>) {
+chomp $_;
+        @@@@
+                print "$_\n";
+        }
+        # match
+        elsif(m/.*\.apk/i){
+                print "$_\n";
+        }
+        else{
+                ####
+        }
+}
